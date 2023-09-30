7487384955da65acb4a22cfc16eaeffb63aae4d2
YoungTae0406 <43427176+YoungTae0406@users.noreply.github.com>
2023-10-01 00:25:41 +0900

    feat(PostsDateTime) : add BaseTimeEntity and implement createdDate, modifiedDate Test.

--- src/main/java/com/start/spring_boot/domain/posts/Posts.java
+++ src/main/java/com/start/spring_boot/domain/posts/Posts.java
@@ -1,5 +1,6 @@
 package com.start.spring_boot.domain.posts;
 
+import com.start.spring_boot.domain.BaseTimeEntity;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,7 +15,7 @@ import javax.persistence.Id;
 @NoArgsConstructor
 @Entity // JPA에서 제공하는 어노테이션. 테이블과 링크될 클래스임을 나타냄. 기본값으로 클래스의 카멜케이스 이름을 언더스코어 네이밍으로
 // 테이블 이름을 매칭합니다. ex) SalesManager.java -> sales_manager table
-public class Posts {
+public class Posts extends BaseTimeEntity {
     @Id // JPA에서 제공하는 어노테이션. 해당 테이블의 PK 필드를 나타냅니다.
     @GeneratedValue(strategy = GenerationType.IDENTITY) // JPA에서 제공하는 어노테이션. PK의 생성 규칙을 나타낸다. 이 옵션을 추가하면
     // auto_increment가 된다.
